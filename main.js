f9aa6249dda0debe5c22abd44e62264a4e31b193
Mahliyo <gasandigital13@gmail.com>
2026-04-05 14:56:00 +0500

    javascriptda button

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ function crudList (){
     list.innerHTML = data.map((item, i) => `
     <li>
       ${item}
+      <div>
+          <button onclik ="edit(${i})></button>
+          <button onclik ="edit(${i})></button>
+            </div>
        </li>
     `)
 }
